4256b94be9ea5d0ccee120477417fb3b960be0e4
Endeyr <aaron.thompson42594@gmail.com>
2022-05-31 12:47:00 -0400

    created playRound and playPlayer functions.

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ function computerPlay() {
     return "Error"
   }
 }
+
+function playerPlay() {
+  let userValue = prompt("Pick rock, paper, or scissors: ")
+  return userValue.toLowerCase()
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "tie"
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beat paper."
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose. Paper beats rock."
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats rock."
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose. Rock beats scissors"
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors"
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose. Scissors beat paper"
+  } else {
+    return "Error: Choose a correct option"
+  }
+}
+const playerSelection = playerPlay()
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
+
+// function game() {
+// playRound()
+// for (let i = 0; i < 5; i++) {
+// your code here!
+// }}
