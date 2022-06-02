5b3b028912cbddc376013920367c0f9fb9e43399
Endeyr <aaron.thompson42594@gmail.com>
2022-06-02 12:46:04 -0400

    removed old files.

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 let userScore = 0
 let computerScore = 0
+let playing = false
+const compSelect = document.getElementById("computerSelection")
+const results = document.getElementById("results")
+const score = document.getElementById("score")
+const btns = document.getElementById("buttons")
+const end = document.getElementById("end")
+const again = document.getElementById("again")
+
+function setFire() {
+  let f = "fire"
+  playRound(f)
+}
+function setWater() {
+  let w = "water"
+  playRound(w)
+}
+function setGrass() {
+  let g = "grass"
+  playRound(g)
+}
 function playGame() {
   while (userScore < 5 && computerScore < 5) {
-    let userChoice = prompt("Choose fire, water, or grass: ").toLowerCase()
-    let computerChoice = computerPlay()
-    if (userChoice + computerChoice === "fire" + "grass") {
-      win()
-    } else if (userChoice + computerChoice === "grass" + "water") {
-      win()
-    } else if (userChoice + computerChoice === "water" + "fire") {
-      win()
-    } else if (userChoice + computerChoice === "water" + "grass") {
-      lose()
-    } else if (userChoice + computerChoice === "grass" + "fire") {
-      lose()
-    } else if (userChoice + computerChoice === "fire" + "water") {
-      lose()
-    } else {
-      tie()
-    }
-  }
-  if (userScore > computerScore) {
-    return "You Win! " + userScore + " : " + computerScore
-  } else if (userScore < computerScore) {
-    return "You lose " + userScore + " : " + computerScore
+    playRound()
   }
 }
+function playRound(userChoice) {
+  let computerChoice = computerPlay()
+
+  if (userChoice + computerChoice === "fire" + "grass") {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    win()
+  } else if (userChoice + computerChoice === "grass" + "water") {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    win()
+  } else if (userChoice + computerChoice === "water" + "fire") {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    win()
+  } else if (userChoice + computerChoice === "water" + "grass") {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    lose()
+  } else if (userChoice + computerChoice === "grass" + "fire") {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    lose()
+  } else if (userChoice + computerChoice === "fire" + "water") {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    lose()
+  } else if (userChoice === computerChoice) {
+    compSelect.textContent = "Computer Selected: " + computerChoice
+    tie()
+  } else console.log("ERROR")
+  if (userScore >= 5) {
+    end.textContent = "You Win! " + userScore + " : " + computerScore
+    let pa = document.createElement("button")
+    pa.textContent = "Play Again"
+    pa.id = "pa"
+    pa.onClick = "replayGame()"
+    again.appendChild(pa)
+  } else if (computerScore >= 5) {
+    end.textContent = "You lose " + userScore + " : " + computerScore
+    let pa = document.createElement("button")
+    pa.textContent = "Play Again"
+    pa.id = "pa"
+    again.appendChild(pa)
+    pa.addEventListener("click", replayGame)
+  }
+}
+
 // computer random choice
 function computerPlay() {
   const fwg = ["fire", "water", "grass"]
@@ -34,12 +75,27 @@ function computerPlay() {
 }
 function win() {
   userScore++
-  return "You win " + userScore + " : " + computerScore
+  results.textContent = "You win!"
+  score.textContent =
+    "User Score: " + userScore + " - " + " Computer Score: " + computerScore
+  console.log("you win")
 }
 function lose() {
   computerScore++
-  return "You lose " + userScore + " : " + computerScore
+  results.textContent = "You lose"
+  score.textContent =
+    "User Score: " + userScore + " - " + " Computer Score: " + computerScore
+  console.log("you lose")
 }
 function tie() {
-  return "You tie " + userScore + " : " + computerScore
+  results.textContent = "You tie!"
+  score.textContent =
+    "User Score: " + userScore + " - " + " Computer Score: " + computerScore
+  console.log("you tie")
+}
+function replayGame() {
+  location.reload()
+}
+window.onload = function () {
+  let startGame = document.getElementById("startGame")
 }
